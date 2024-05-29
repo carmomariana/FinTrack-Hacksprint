8baf16c5ed6753acc53bb3419fb2eec30eb57e69
Acer <evelyn.designer@gmail.com>
2024-05-29 10:43:20 -0300

    Alteração da tela de DespesaActivity.kt

--- app/src/main/java/com/example/fintrack/DespesaActivity.kt
+++ app/src/main/java/com/example/fintrack/DespesaActivity.kt
@@ -1,35 +1,50 @@
 package com.example.fintrack
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.fintrack.databinding.ActivityDespesaBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class DespesaActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDespesaBinding
+    //  private lateinit var binding: ActivityDespesaBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityDespesaBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        initListeners()
-    }
+        setContentView(R.layout.activity_despesa)
 
-    private fun initListeners() {
-        binding.btnSave.setOnClickListener {
+        val edtDespesa = findViewById<EditText>(R.id.edtDespesa)
+        val recCategory = findViewById<RecyclerView>(R.id.tvCategoria)
+        val edtPrice = findViewById<EditText>(R.id.edtPrice)
+        val btnCreate = findViewById<Button>(R.id.btnSave)
 
+        btnCreate.setOnClickListener {
+
+            val StrDepesa: String = edtDespesa.text.toString()
+            val StrPrice: String = edtPrice.text.toString()
+            val StrCategory: String = recCategory.toString()
+
+            if (StrDepesa.isBlank() || StrPrice.isBlank()|| StrCategory.isBlank()) {
+                Snackbar.make(edtDespesa, "Preencha todos os campos! ", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+
+                val Despesa = edtDespesa.text
+                val Price = edtPrice.text
+                val Categoria = recCategory.toString()
+
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("KEY_DESPESA", Despesa)
+                intent.putExtra("KEY_PRICE", Price)
+                intent.putExtra("KEY_CATEGORIA", Categoria)
+                startActivity(intent)
+            }
         }
     }
 }
